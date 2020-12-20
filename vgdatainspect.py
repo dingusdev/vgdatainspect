67bdf4008f16e61acfbd3df90595b2e1278e8ecc
dingusdev <52434309+dingusdev@users.noreply.github.com>
2020-12-20 16:07:35 -0700

    Initial source commit

---
+++ vgdatainspect.py
@@ -0,0 +1,59 @@
+# VGDataInspect
+# Written by Divingkatae on 12/16/2020-12/20/2020
+
+import os
+import sys
+import json
+
+def check_wad(file_input):
+    if ((file_input[0:8] == "49574144") or (file_input[0:8] == "50574144")):
+        return "Doom WAD file confirmed"
+        
+def check_xld(file_input):
+    if (file_input[0:10] == "584C443049"):
+        return "Albion XLD file confirmed" 
+    
+def grab_extension_match(argument): 
+    with open('rcogfileexts.json', 'r') as json_file:
+        switcher = json.load(json_file)
+  
+    return switcher.get(argument, "Unrecognized File Extension") 
+
+if __name__ == "__main__":
+    txt = input("Enter the file you would like to inspect: ")
+    argument=os.path.splitext(txt)
+    print (argument[1])
+    print (grab_extension_match(argument[1]))
+
+    if os.path.exists(txt):
+        grabfile = open(txt, 'rb')
+        filesize = os.stat(txt).st_size
+        print("File Size (in bytes):", filesize)
+        if (filesize >= 256):
+            print("First 256 bytes:")
+            step1 = grabfile.read()[0:256].hex()
+
+            # Kludge - Signature check
+            
+            if (argument[1] == ".wad"):
+                print(check_wad(step1))
+            elif (argument[1] == ".xld"):
+                print(check_xld(step1))
+
+            for sub_offset in range(0, 512, 32):
+                print(step1[sub_offset:(sub_offset +32)].upper())
+        else:
+            print("Entire File Contents:")
+            step1 = grabfile.read().hex()
+
+            # Kludge - Signature check
+            
+            if (argument[1] == ".wad"):
+                print(check_wad(step1))
+            elif (argument[1] == ".xld"):
+                print(check_xld(step1))
+                
+            print(step1.upper())
+    else:
+        print("It seems the file does not exist. Make sure it is in the proper file path specified.")
+    
